b1b1e8812cb046717315c433bf87fa91a90fb470
Ethan <riley787cmplex@gmail.com>
2016-01-14 22:55:19 +0000

    finsihed coursel

--- app/js/util.js
+++ app/js/util.js
@@ -1,24 +1,45 @@
 "use strict"
 window.$ = window.jQuery = require("jquery")
+
 function fixTileSquareHeight(){
   if($(".square-tile")[0]){
     var width = $(".square-tile").width().toString();
     $(".square-tile").css("height", width+"px");
   }
 }
-
+  var slide =0;
 function nextSlide(){
-  $(".onscreen").css("left", "200vw");
-  $( ".slide").css("left", "0");
+  var slides = $(".slide").size();
+
+
+  $(".slide.onscreen").addClass("leftoffscreen");
+  $(".slide.onscreen.leftoffscreen").removeClass("onscreen");
+
+  ++slide;
+  slide = slide%slides;
+
+  $(".slide:nth-of-type("+(slide+1).toString()+")").addClass("onscreen");
 }
 
 function prevSlide(){
+  var slides = $(".slide").size();
+
+  $(".slide.onscreen").removeClass("onscreen");
+
+  --slide;
+  slide=((slide%slides)+slides)%slides;
+  console.log(slide);
+
+  $(".slide:nth-of-type("+(slide+1).toString()+")").addClass("onscreen");
+  $(".slide.onscreen").removeClass("leftoffscreen");
 
 }
 
 $(document).ready(
   function(){
     fixTileSquareHeight();
+    $(".slide-button.right").click(nextSlide);
+    $(".slide-button.left").click(prevSlide);
   }
 )
 
