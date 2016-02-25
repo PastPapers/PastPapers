5ac8e10c6807e3de333376f81a1ff1ce61be73ee
Ethan <riley787cmplex@gmail.com>
2016-02-25 19:25:49 +0000

    get out of callback hell :P

--- app/js/pdfHandler.js
+++ app/js/pdfHandler.js
@@ -14,35 +14,36 @@ window.$ = window.jQuery = require("jquery");
     });
   }
 
-  pdfHandler.handlePdfText = function(text){
-    var content = "";
-      for(var v=0; v < text.items.length; v++){
-          if(typeof(text.items[v].str) !== "undefined"){
-            if(typeof(content) !== "undefined"){
-              content += " " + text.items[v].str;
-            }
-            else{
-              content = text.items[v].str;
-            }
-          }
-        }
-      return content;
-    }
-
   // warning: does not get images.
   //@throwable: if fails getting document will throw error.
   pdfHandler.getPdfContent = function(file){
-    var Content = [];
-    pdf.getDocument(file).then(function(doc){;
-      promises = [];
-      for(var i =0; i < doc.numPages; i++){
-        promises.push(doc.getPage(i));
+    return pdf.getDocument(file).then(function(doc){
+      var promises = [];
+      for(var i =1; i < doc.numPages; i++){ promises.push(doc.getPage(i)
+			.then(function(page){
+				return page.getTextContent().then(function(text){
+					var content = "";
+					for(var v=0; v < text.items.length; v++){
+						if(typeof(text.items[v].str) !== "undefined"){
+							if(typeof(content) !== "undefined"){
+							  content += " " + text.items[v].str;
+							}
+							else{
+							  content = text.items[v].str;
+							}
+						}
+					}
+					return content;
+				});	
+			}	
+		));
       }
-      return Promise.all(promises);
+
+	  return Promise.all(promises);
     },
     function(err){
       throw err;
-    })
+	});
   }
 
 }(window.pdfHandler = window.pdfHandler || {}, jQuery, pdf));
